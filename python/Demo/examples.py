46a2a7c26d808951eca789507379d5cd6e9220a9
jhuang_instahdr <jason.huang@instahdr.com>
2023-09-27 00:26:14 -0400

    Use logging instead of printing for the examples

--- python/Demo/examples.py
+++ python/Demo/examples.py
@@ -2,6 +2,7 @@ from Units.LengthUnits import Metre, Foot
 from Unitless.Scalars import Kilo
 from Model.NumericUnitTypes import LengthUnit, TimeUnit
 from Model.NumericValueWithUnit import NumericValueWithUnit    
+from logging import info, error
 
 def Examples() -> None:
     #scalar modifiers
@@ -13,30 +14,30 @@ def Examples() -> None:
     foot = Foot()
 
     value = NumericValueWithUnit(10,metre,LengthUnit)
-    print(value.Convert(kilometre))
-    print(value.Convert(foot))
-    print(value.Convert(metre))
+    info(value.Convert(kilometre))
+    info(value.Convert(foot))
+    info(value.Convert(metre))
 
     # error should be raised because metre is not a TimeUnit
     try:
-        invalid = NumericValueWithUnit(10,metre,TimeUnit)
+        _ = NumericValueWithUnit(10,metre,TimeUnit)
     except TypeError as type_error:
-        print(f"{type_error}")
+        error(f"{type_error}")
 
     # error should be raised because '10' is not a numeric value
     try:
-        invalid = NumericValueWithUnit('10',metre,TimeUnit)
+        _ = NumericValueWithUnit('10',metre,TimeUnit)
     except TypeError as type_error:
-        print(f"{type_error}")
+        error(f"{type_error}")
 
     # error should be raised because LengthUnit cannot convert to a different unit type or a unitless quantity
     try:
         value.Convert(kilo)
     except TypeError as type_error:
-        print(f"{type_error}")
+        error(f"{type_error}")
 
     # error should be raised because metre is not a valid unitless modifier
     try:
         invalid = Metre(metre)
     except TypeError as type_error:
-        print(f"{type_error}")
+        error(f"{type_error}")
